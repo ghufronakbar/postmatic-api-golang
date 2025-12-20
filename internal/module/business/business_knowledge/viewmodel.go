94dee02117695cf8d669b1f8faced3be6fe20aae
lanstheprodigy <alan.040219.gam@gmail.com>
2025-12-20 22:50:53 +0700

    refactor: update BusinessKnowledgeResponse CreatedAt and UpdatedAt fields to time.Time objects

--- internal/module/business/business_knowledge/viewmodel.go
+++ internal/module/business/business_knowledge/viewmodel.go
@@ -1,16 +1,18 @@
 package business_knowledge
 
+import "time"
+
 type BusinessKnowledgeResponse struct {
-	PrimaryLogoUrl     string  `json:"primaryLogoUrl"`
-	Name               string  `json:"name"`
-	Category           string  `json:"category"`
-	Description        string  `json:"description"`
-	UniqueSellingPoint string  `json:"uniqueSellingPoint"`
-	WebsiteUrl         *string `json:"websiteUrl"`
-	VisionMission      string  `json:"visionMission"`
-	Location           string  `json:"location"`
-	RootBusinessId     string  `json:"rootBusinessId"`
-	CreatedAt          string  `json:"createdAt"`
-	UpdatedAt          string  `json:"updatedAt"`
-	ColorTone          string  `json:"colorTone"`
+	PrimaryLogoUrl     string    `json:"primaryLogoUrl"`
+	Name               string    `json:"name"`
+	Category           string    `json:"category"`
+	Description        string    `json:"description"`
+	UniqueSellingPoint string    `json:"uniqueSellingPoint"`
+	WebsiteUrl         *string   `json:"websiteUrl"`
+	VisionMission      string    `json:"visionMission"`
+	Location           string    `json:"location"`
+	RootBusinessId     string    `json:"rootBusinessId"`
+	CreatedAt          time.Time `json:"createdAt"`
+	UpdatedAt          time.Time `json:"updatedAt"`
+	ColorTone          string    `json:"colorTone"`
 }
